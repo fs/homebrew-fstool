626858a9037d2845a73d8a1178fd4243f7b9cae3
Max Prokopiev <me@maxprokopiev.com>
2014-08-17 12:36:13 +0400

    Release 1.7.0
    https://github.com/fs/fs-tool/releases/tag/1.7.0

--- fs-tool.rb
+++ fs-tool.rb
@@ -2,11 +2,11 @@ require "formula"
 
 class FsTool < Formula
   homepage "http://fs.github.io/fs-tool/"
-  version "1.6.1"
+  version "1.7.0"
 
-  url "https://github.com/fs/fs-tool/archive/1.6.1.tar.gz"
-  sha1 "63b968aaef640ada2db0dbfe163ad8d0d913e007"
-  
+  url "https://github.com/fs/fs-tool/archive/1.7.0.tar.gz"
+  sha1 "45ec4be136c6c149c2895d9841e4ebf605a1bcf5"
+
   def install
     bin.install Dir['bin/**']
     libexec.install Dir['libexec/**']
